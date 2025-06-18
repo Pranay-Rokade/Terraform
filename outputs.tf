fdea2d3b5244f9fdf5bd490697722e62759c68d5
Pranay Rokade <144474882+Pranay-Rokade@users.noreply.github.com>
2025-06-18 19:52:29 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,15 @@
 output "arn" {
-  value = aws_instance.my_ec2.arn
+  value = aws_instance.my_ec2[*].arn
 }
 
 output "public_ip" {
-  value = aws_instance.my_ec2.public_ip
+  value = aws_instance.my_ec2[*].public_ip
+}
+
+output "public_dns" {
+  value = aws_instance.my_ec2[*].public_dns
+}
+
+output "private_ip" {
+  value = aws_instance.my_ec2[*].private_ip
 }
